f5f739e819c4b3ab942b29c040b07c87298593a1
Dmitrii Kupriunin <dmitrii_kupriunin@epam.com>
2024-05-08 19:28:47 +0200

    feat(MCA-29): add back button to the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { BackButton } from '../BackButton';
+import { cn } from '../../helpers';
 
 export const Header: React.FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const shouldRenderBackButton = location.pathname !== '/';
+  const onBackButtonClick = () => {
+    navigate(-1);
+  };
+
   return (
     <header className="pt-8">
-      <Link to="/">
-        <h1 className="text-2xl ml-4 mb-2 text-primary-blue">Podcaster</h1>
-      </Link>
+      <div className="flex items-center mb-2">
+        <BackButton
+          onClick={onBackButtonClick}
+          fillColor="#252659"
+          className={cn({ invisible: !shouldRenderBackButton }, 'ml-2')}
+        />
+        <Link to="/" className="ml-6">
+          <h1 className="text-2xl text-primary-blue">Podcaster</h1>
+        </Link>
+      </div>
       <hr />
     </header>
   );
