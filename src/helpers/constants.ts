f1c564aad1234f4598ba48a2ae23577e15ad89f1
ololosha <49726944+dimalovesbomb@users.noreply.github.com>
2024-05-08 19:04:26 +0200

    feat(MCA-28): replace audio player (#56)

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,2 +1,4 @@
 export const LC_OVERVIEW_DATA_KEY = 'MCA-OVERVIEW-DATA';
 export const LC_QUOTA_EXCEEDED_ERROR = 'QuotaExceededError';
+
+export const SET_DATA_FOR_PLAYER = 'set-data-for-player';
--- src/pages/EpisodePage/EpisodePage.tsx
+++ src/pages/EpisodePage/EpisodePage.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useGetData, useGetDetails } from '../../hooks';
 import { PodcastCard } from '../../components/PodcastCard';
 import DOMPurity from 'isomorphic-dompurify';
-
-import './EpisodePageStyles.css';
+import { SET_DATA_FOR_PLAYER } from '../../helpers/constants';
 
 export const EpisodePage: React.FC = () => {
   const { podcastId, episodeId } = useParams();
@@ -13,8 +12,14 @@ export const EpisodePage: React.FC = () => {
   const { details } = useGetDetails(podcastId);
   const currentEpisode = details?.items.find((episode) => episode.created === parseInt(atob(episodeId || '')));
 
+  useEffect(() => {
+    if (currentEpisode && currentPodcast) {
+      document.dispatchEvent(new CustomEvent(SET_DATA_FOR_PLAYER, { detail: { currentEpisode, currentPodcast } }));
+    }
+  }, [currentEpisode, currentPodcast]);
+
   return (
-    <main className="flex mx-auto mt-8 desktop:max-w-[1050px] max-w-[1000px] flex-wrap desktop:flex-nowrap justify-center desktop:justify-normal gap-12">
+    <main className="flex mx-auto mt-8 desktop:max-w-[1050px] max-w-[1000px] flex-wrap desktop:flex-nowrap justify-center desktop:justify-normal gap-12 desktop:h-screen">
       <section>
         <PodcastCard
           image={{ src: currentPodcast?.['im:image'][2].label || '', alt: currentPodcast?.['im:artist'].label || '' }}
@@ -28,9 +33,6 @@ export const EpisodePage: React.FC = () => {
         <div className="rounded p-4 min-w-[25vw] shadow">
           <h2>{currentEpisode?.title}</h2>
           <div dangerouslySetInnerHTML={{ __html: DOMPurity.sanitize(currentEpisode?.content || '') }} />
-          <div className="flex-center rounded-full p-1 bg-primary-blue mt-16">
-            <audio className="w-full" controls src={currentEpisode?.enclosures[0].url} controlsList="nodownload" />
-          </div>
         </div>
       </section>
     </main>
