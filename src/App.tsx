aada845b172ef2b0e473a74442522eee13383f35
Dmitrii Kupriunin <dmitrii_kupriunin@epam.com>
2024-05-15 19:01:18 +0200

    feat(MCA-31): webpack - split app onto chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Header } from './components/Header';
 import { AudioPlayer } from './components/AudioPlayer';
 
 import './globals.css';
@@ -9,7 +8,6 @@ import './styles.css';
 export default function App() {
   return (
     <div className="max-w-[1100px] mx-auto">
-      <Header />
       <Outlet />
       <AudioPlayer className="sticky bottom-0" />
     </div>
