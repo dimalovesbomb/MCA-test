4d5fd0c955bbd42789f55c8576053fc6409b1e94
ololosha <49726944+dimalovesbomb@users.noreply.github.com>
2024-05-15 19:02:30 +0200

    feat(MCA-31): webpack - split app onto chunks (#62)

--- src/pages/EpisodePage/EpisodePage.tsx
+++ src/pages/EpisodePage/EpisodePage.tsx
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useGetData, useGetDetails } from '../../hooks';
 import { PodcastCard } from '../../components/PodcastCard';
+import { Header } from '../../components/Header';
 import DOMPurity from 'isomorphic-dompurify';
 import { SET_DATA_FOR_PLAYER } from '../../helpers/constants';
 
-export const EpisodePage: React.FC = () => {
+const EpisodePage: React.FC = () => {
   const { podcastId, episodeId } = useParams();
   const { findPodcast } = useGetData();
   const currentPodcast = findPodcast(podcastId || '');
@@ -19,22 +20,27 @@ export const EpisodePage: React.FC = () => {
   }, [currentEpisode, currentPodcast]);
 
   return (
-    <main className="flex mx-auto mt-8 desktop:max-w-[1050px] max-w-[1000px] flex-wrap desktop:flex-nowrap justify-center desktop:justify-normal gap-12 desktop:h-screen">
-      <section>
-        <PodcastCard
-          image={{ src: currentPodcast?.['im:image'][2].label || '', alt: currentPodcast?.['im:artist'].label || '' }}
-          label={currentPodcast?.['im:name'].label || ''}
-          artist={currentPodcast?.['im:artist'].label || ''}
-          summary={currentPodcast?.summary.label || ''}
-          url={currentPodcast?.link}
-        />
-      </section>
-      <section>
-        <div className="rounded p-4 min-w-[25vw] shadow">
-          <h2>{currentEpisode?.title}</h2>
-          <div dangerouslySetInnerHTML={{ __html: DOMPurity.sanitize(currentEpisode?.content || '') }} />
-        </div>
-      </section>
-    </main>
+    <>
+      <Header />
+      <main className="flex mx-auto mt-8 desktop:max-w-[1050px] max-w-[1000px] flex-wrap desktop:flex-nowrap justify-center desktop:justify-normal gap-12 desktop:h-screen">
+        <section>
+          <PodcastCard
+            image={{ src: currentPodcast?.['im:image'][2].label || '', alt: currentPodcast?.['im:artist'].label || '' }}
+            label={currentPodcast?.['im:name'].label || ''}
+            artist={currentPodcast?.['im:artist'].label || ''}
+            summary={currentPodcast?.summary.label || ''}
+            url={currentPodcast?.link}
+          />
+        </section>
+        <section>
+          <div className="rounded p-4 min-w-[25vw] shadow">
+            <h2>{currentEpisode?.title}</h2>
+            <div dangerouslySetInnerHTML={{ __html: DOMPurity.sanitize(currentEpisode?.content || '') }} />
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
+
+export default EpisodePage;
